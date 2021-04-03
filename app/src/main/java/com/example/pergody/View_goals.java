110ada7e05f1a38abb31ba5032fe1483114798d8
Chatch520 <chatch520@gmail.com>
2021-04-02 18:40:27 -0600

    Added the view goals page, finished adding the "view" page going to go back to work figuring out how to retrieve the data.

--- app/src/main/java/com/example/pergody/View_goals.java
+++ app/src/main/java/com/example/pergody/View_goals.java
@@ -1,6 +1,25 @@
 package com.example.pergody;
 
-public class View_goals {
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class View_goals extends AppCompatActivity {
+
+    @Override
+    public void onCreate (Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.goal_list);
+    }
+
+
 
 }
+
+
+
 
